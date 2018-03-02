053fe8713ee101c5cb8252cbdbd8788e3e94e9c9
Sergey Bulavkin <BulavkinSM@msk.bcs.ru>
2018-03-02 17:50:08 +0300

    init

---
+++ src/test/java/com/example/demo/FailedTests.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class FailedTests {
+
+    @Autowired
+    WebTestClient client;
+
+    @Test
+    public void getAllMessagesShouldBeOk() {
+        client.get().uri("/").exchange().expectStatus().isOk();
+    }
+
+}
